5b12359b6b4e9de8fcab7604a664353473223b0d
George Chatzisofroniou <sophron@latthi.com>
2014-07-01 01:20:16 +0300

    A very first database interface.

---
+++ node/db_store.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+#
+# This library is free software, distributed under the terms of
+# the GNU Lesser General Public License Version 3, or any later version.
+# See the COPYING file included in this archive
+#
+# The docstrings in this module contain epytext markup; API documentation
+# may be created by processing this file with epydoc: http://epydoc.sf.net
+
+import sqlite3
+
+# TODO: Grab this from constants file.
+DBFILE = "db/ob.db"
+
+class Obdb():
+    """ Interface for db storage. Serves as segregation of the persistence layer 
+    and the application logic
+    """ 
+    def __init__(self):
+        self.con = False
+
+    def _connectToDb(self):
+        """ Opens a db connection
+        """
+        self.con = sqlite3.connect(DBFILE)
+        self.con.row_factory = self._dictFactory
+
+    def _disconnectFromDb(self):
+        """ Close the db connection
+        """
+        if self.con:
+           self.con.close()       
+        self.con = False
+ 
+    def _dictFactory(self, cursor, row):
+        """ A factory that allows sqlite to return a dictionary instead of a tuple
+        """
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
+    def updateEntries(self, table, where_dict, set_dict, operator="AND"):
+        """ A wrapper for the SQL UPDATE operation
+        @param table: The table to search to
+        @param whereDict: A dictionary with the WHERE clauses
+        @param setDict: A dictionary with the SET clauses
+        """
+        self._connectToDb()
+        with self.con:
+            cur = self.con.cursor()
+            first = True
+            for key, value in set_dict.iteritems():
+                if first:
+                    set_part = "%s = '%s'" % (key, value)
+                    first = False
+                else:
+                    set_part = set_part + ", %s = '%s'" % (key, value)
+            first = True
+            for key, value in where_dict.iteritems():
+                if first: 
+                    where_part = "%s = '%s'" % (key, value)
+                    first = False
+                else:
+                    where_part = where_part + ", %s = '%s'" % (key, value)
+            query = "UPDATE %s SET %s WHERE %s" \
+                    % (table, set_part, where_part)
+            cur.execute(query)
+        self._disconnectFromDb()
+
+    def insertEntry(self, table, update_dict):
+        """ A wrapper for the SQL INSERT operation
+        @param table: The table to search to
+        @param updateDict: A dictionary with the values to set
+        """
+        self._connectToDb()
+        with self.con:
+            cur = self.con.cursor()
+            first = True
+            for key, value in update_dict.iteritems():
+                if first: 
+                    updatefield_part = "%s" % (key)
+                    setfield_part = "'%s'" % (value)
+                    first = False
+                else:
+                    updatefield_part = updatefield_part + ", %s" % (key)
+                    setfield_part = setfield_part + ", '%s'" % (value)
+            query = "INSERT INTO %s(%s) VALUES(%s)"  \
+                    % (table, updatefield_part, setfield_part)
+            cur.execute(query)
+        self._disconnectFromDb()
+
+    def selectEntries(self, table, where_dict={"'1'":"1"}):
+        """ A wrapper for the SQL SELECT operation. It will always return all the 
+            attributes for the selected rows.
+        @param table: The table to search to
+        @param whereDict: A dictionary with the WHERE clauses. If ommited it will 
+        return all the rows of the table
+        """
+        self._connectToDb()
+        with self.con:
+            cur = self.con.cursor()
+            first = True
+            for key, value in where_dict.iteritems():
+                if first: 
+                    where_part = "%s = '%s'" % (key, value)
+                    first = False
+                else:
+                    where_part = where_part + ", %s = '%s'" % (key, value)
+            query = "SELECT * FROM %s WHERE %s" \
+                    % (table, where_part)
+            cur.execute(query)
+            rows = cur.fetchall()                
+        self._disconnectFromDb()
+        return rows
+
+    def deleteEntries(self, table, where_dict={"'1'":"1"}):
+        """ A wrapper for the SQL DELETE operation. It will always return all the 
+            attributes for the selected rows.
+        @param table: The table to search to
+        @param whereDict: A dictionary with the WHERE clauses. If ommited it will 
+        delete all the rows of the table
+
+        """
+
+        self._connectToDb()
+        with self.con:
+            cur = self.con.cursor()
+            first = True
+            for key, value in where_dict.iteritems():
+                if first: 
+                    where_part = "%s = '%s'" % (key, value)
+                    first = False
+                else:
+                    where_part = where_part + ", %s = '%s'" % (key, value)
+            query = "DELETE FROM %s WHERE %s" \
+                    % (table, where_part)
+            cur.execute(query)
+        self._disconnectFromDb()
