84e655d609196c624a240dbfd250f79eeffa214b
George Chatzisofroniou <sophron@latthi.com>
2014-07-01 23:31:02 +0300

    Migrated orders module from mongo to sqlite.

--- node/orders.py
+++ node/orders.py
@@ -6,6 +6,7 @@ from protocol import order
 from pyelliptic import ECC
 from pymongo import MongoClient
 from multisig import Multisig
+from db_store import Obdb
 
 
 class Orders(object):
@@ -17,64 +18,43 @@ class Orders(object):
         self._escrows = [
             "02ca0020a9de236b47ca19e147cf2cd5b98b6600f168481da8ec0ca9ec92b59b76db1c3d0020f9038a585b93160632f1edec8278ddaeacc38a381c105860d702d7e81ffaa14d",
             "02ca0020c0d9cd9bdd70c8565374ed8986ac58d24f076e9bcc401fc836352da4fc21f8490020b59dec0aff5e93184d022423893568df13ec1b8352e5f1141dbc669456af510c"]
-
-        MONGODB_URI = 'mongodb://localhost:27017'
-        _dbclient = MongoClient()
-        self._db = _dbclient.openbazaar
+        self._db = Obdb()
         self._orders = self.get_orders()
-        self.orders = self._db.orders
-
         order_transport.add_callback('order', self.on_order)
         self._log = logging.getLogger(self.__class__.__name__)
 
     def get_order(self, orderId):
 
-        _order = self._db.orders.find_one({"id": orderId})
-
-        # Get order prototype object before storing
-        order = {"id": _order['id'],
-                 "state": _order['state'],
-                 "address": _order['address'] if _order.has_key("address") else "",
-                 "buyer": _order['buyer'] if _order.has_key("buyer") else "",
-                 "seller": _order['seller'] if _order.has_key("seller") else "",
-                 "escrows": _order['escrows'] if _order.has_key("escrows") else "",
-                 "text": _order['text'] if _order.has_key("text") else "",
-                 "created": _order['created'] if _order.has_key("created") else ""}
-        # orders.append(_order)
-
-
+        order = self._db.selectEntries("orders", {"id": orderId})[0]
+        order["escrows"] = self._db.selectEntries("escrows", {"id": order["id"]}) 
+        order_escrows = ""
+        for e in order["escrows"]:
+            for key, val in e.iteritems():
+                order_escrows = order_escrows + ", " + val
+        order["escrows"] = order_escrows
         return order
 
-    def get_orders(self):
-        orders = []
-        for _order in self._db.orders.find().sort([("created", -1)]):
-            # Get order prototype object before storing
-            orders.append({"id": _order['id'],
-                           "state": _order['state'],
-                           "address": _order['address'] if _order.has_key("address") else "",
-                           "buyer": _order['buyer'] if _order.has_key("buyer") else "",
-                           "seller": _order['seller'] if _order.has_key("seller") else "",
-                           "escrows": _order['escrows'] if _order.has_key("escrows") else "",
-                           "text": _order['text'] if _order.has_key("text") else "",
-                           "created": _order['created'] if _order.has_key("created") else ""})
-            # orders.append(_order)
+    def get_orders:
 
+        orders = self._db.selectEntries("orders", order_field="created", order="DESC")
+        for o in orders:
+            o["escrows"] = self._db.selectEntries("escrows", {"id": o["id"]})
         return orders
-
 
     # Create a new order
     def create_order(self, seller, text):
         self._log.info('CREATING ORDER')
         id = random.randint(0, 1000000)
         buyer = self._transport._myself.get_pubkey()
-        new_order = order(id, buyer, seller, 'new', text, self._escrows)
+        new_order = order(id, buyer, seller, 'new', text)
 
         # Add a timestamp
         new_order['created'] = time.time()
 
         self._transport.send(new_order, seller)
-
-        self._db.orders.insert(new_order)
+        self._db.insertEntry("orders", new_order)
+        for escrow in self._escrows:
+            self._db.getOrCreate("escrows", {"address": escrow}) 
 
 
     def accept_order(self, new_order):
@@ -92,26 +72,24 @@ class Orders(object):
         self._multisig = Multisig(None, 2, [buyer, seller, escrow])
 
         new_order['address'] = self._multisig.address
-
-        self._db.orders.update({"id": new_order['id']}, {"$set": new_order}, True)
-
+        self._db.updateEntries("orders", {"id": new_order['id']}, {new_order})
         self._transport.send(new_order, new_order['buyer'].decode('hex'))
 
     def pay_order(self, new_order):  # action
         new_order['state'] = 'paid'
-        self._db.orders.update({"id": new_order['id']}, {"$set": new_order}, True)
+        self._db.updateEntries("orders",{"id": new_order['id']},new_order)
         new_order['type'] = 'order'
         self._transport.send(new_order, new_order['seller'].decode('hex'))
 
     def send_order(self, new_order):  # action
         new_order['state'] = 'sent'
-        self._db.orders.update({"id": new_order['id']}, {"$set": new_order}, True)
+        self._db.updateEntries("orders",{"id": new_order['id']},new_order)
         new_order['type'] = 'order'
         self._transport.send(new_order, new_order['buyer'].decode('hex'))
 
     def receive_order(self, new_order):  # action
         new_order['state'] = 'received'
-        self._db.orders.update({"id": new_order['id']}, {"$set": new_order}, True)
+        self._db.updateEntries("orders",{"id": new_order['id']},new_order)
         self._transport.send(new_order, new_order['seller'].decode('hex'))
 
 
@@ -153,7 +131,7 @@ class Orders(object):
                 self._log.info("Bad subjects [%s]" % state)
             elif myself == buyer:
                 # wait for confirmation
-                self._db.orders.update({"id": msg['id']}, {"$set": msg}, True)
+                self._db.updateEntries("orders",{"id": new_order['id']},msg)
                 pass
             else:
                 self._log.info("Order not for us")
