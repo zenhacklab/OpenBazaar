45c3ab9d61f0a440833d21c41d926706d7fb22cb
George Chatzisofroniou <sophron@latthi.com>
2014-07-22 00:27:15 +0300

    Added a condition to be sure.

--- html/js/app.js
+++ html/js/app.js
@@ -1183,8 +1183,12 @@ $scope.BuyItemInstanceCtrl = function ($scope, $modalInstance, myself, merchantP
 
 
     $scope.order = {message:'', tx: '', listingKey:key, listingTotal:'', productTotal:'', productQuantity:1, rawContract:rawContract}
-    //$scope.order.notary = $scope.notaries[0];
-    //$scope.order.arbiter = $scope.arbiters[0];
+    if (typeof $scope.notaries !== 'undefined') {
+        $scope.order.notary = $scope.notaries[0];
+    }
+    if (typeof $scope.arbiters !== 'undefined') {
+        $scope.order.arbiter = $scope.arbiters[0];
+    }
 
     $scope.submitOrder = function() {
 
