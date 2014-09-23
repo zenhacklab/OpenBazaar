c76dc5fd8d3517d84e63c2758b582691d7cc47b3
Giannis Adamopoulos <johnyadams@outlook.com>
2014-09-23 21:11:25 +0300

    Add a few functions to newtork_util.py
    * Add: get_my_ip, is_ipv6_address and get_peer_url.
    * Useful for refactoring transport.py.

--- node/network_util.py
+++ node/network_util.py
@@ -2,6 +2,9 @@ from random import randint
 import re
 import socket
 import struct
+import requests
+from requests.exceptions import RequestException
+from IPy import IPint
 
 
 def is_local_tcp_port_listening(port):
@@ -73,3 +76,36 @@ def uri_parts(uri):
         return m.group(1), m.group(2), m.group(3)
     else:
         raise RuntimeError('URI is not valid')
+
+def get_my_ip():
+    try:
+        r = requests.get('https://icanhazip.com')
+        return r.text.strip()
+    except (AttributeError, RequestException) as e:
+        print '[Requests] error: %s' % e
+    return None
+
+def is_ipv6_address(ip):
+    address = IPint(ip)
+    return address.version == 6
+
+def get_peer_url(address, port):
+    """
+    Returns a url for a peer that can be used by ZMQ
+
+    @param address: A string which can be an IPv4 address, an IPv6 address
+                    or a DNS name
+
+    @param port: the port that will be used to connect to the peer
+    """
+    try:
+        #is_ipv6_address will throw an exception for a DNS name
+        is_ipv6 = is_ipv6_address(address)
+    except  ValueError:
+        is_ipv6 = False
+
+    if is_ipv6:
+        #an IPv6 address must be enclosed in brackets
+        return 'tcp://[%s]:%s' % (address, port)
+    else:
+        return 'tcp://%s:%s' % (address, port)
