5cfd039c1dec90d572a4418a78439f82c902c9c0
Nikolaos Korasidis <renelvon@gmail.com>
2014-09-30 15:38:53 +0300

    Remove unused third-party-tools folder.

--- third-party-tools/import-from-ebay-button/ebay-button.py
+++
@@ -1,8 +0,0 @@
-###############################################################################
-#
-#   Import from eBay listing Button
-#       Generates an OpenBazaar contract from an eBay.com listing.
-#
-###############################################################################
-
-# from ebaysdk import *
